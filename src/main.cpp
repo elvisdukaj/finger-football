7714e7fbb78d3bf9f93d8450ec65a8e8d74d5e7b
Elvis Dukaj <elvis.dukaj@gmail.com>
2017-11-22 18:46:31 +0100

    added imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+//#include <GLES3/gl3.h>
 #include <GL/gl3w.h>
 #include <SDL2/SDL.h>
+#include <imgui.h>
+#include "imgui_impl_sdl_gl3.h"
 using namespace std;
 
 void initGL()
 {
     gl3wInit();
     glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
-    SDL_GL_SetSwapInterval(1);
 }
 
 void draw(SDL_Window* window)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
     SDL_GL_SwapWindow(window);
 }
 
@@ -22,8 +24,10 @@ int main()
 
     SDL_Init(SDL_INIT_VIDEO);
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+//    SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
@@ -36,6 +40,8 @@ int main()
                 );
     auto context = SDL_GL_CreateContext(window);
 
+//    ImGui_ImplSdlGL3_Init(window);
+
     initGL();
 
     SDL_Event event;
@@ -43,8 +49,9 @@ int main()
 
     while(!canExit)
     {
-        if (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event))
         {
+//            ImGui_ImplSdlGL3_ProcessEvent(&event);
             if (event.type == SDL_QUIT)
                 canExit = true;
 
@@ -55,7 +62,13 @@ int main()
             }
         }
 
+//        ImGui_ImplSdlGL3_NewFrame(window);
+//        ImGui::Begin("A Window", nullptr);
+//        ImGui::Text("Hello from another window!");
+//        ImGui::End();
+
         draw(window);
+//        ImGui::Render();
     }
 
     SDL_GL_DeleteContext(context);
@@ -64,4 +77,4 @@ int main()
 
     cout << "Everything is clean now!" << endl;
     return EXIT_SUCCESS;
-} 
+}
